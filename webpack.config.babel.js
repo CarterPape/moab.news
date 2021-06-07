d8149a0d3c9070f91f71184cb81bc0557b890108
Carter Pape <github.com@carterpape.com>
2021-06-07 09:46:49 -0600

    formats webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,21 @@
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import {
+    resolve,
+    dirname
+} from 'path';
+
+import {
+    fileURLToPath
+} from 'url';
 
 import pkg from "dotenv-webpack";
 const Dotenv = pkg;
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const __dirname = dirname(
+    fileURLToPath(
+        import.meta.url
+    )
+);
 
 export default {
     entry: "./webpack/main.js",
